1976ac5218c6ecb2e3c1b4f3e98930d4bcbb3d61
Andrey Shelkovnikov <shelkov09@gmil.com>
2017-10-18 22:13:41 +0300

    tune up HyperCube

--- src/components/HyperCube.js
+++ src/components/HyperCube.js
@@ -11,6 +11,7 @@ const HyperCube = ({
   width,
   height,
   size,
+  margin, // 0..1
 }) => {
   const cubes = [];
 
@@ -20,9 +21,9 @@ const HyperCube = ({
         cubes.push(
           <Cube
             key={`${x}-${y}-${z}`}
-            posX={x-depth/2}
-            posY={y-height/2}
-            posZ={z-width/2}
+            posX={x-depth*(1+margin)/2 + margin*(x+1)}
+            posY={y-height*(1+margin)/2 + margin*(y+1)}
+            posZ={z-width*(1+margin)/2 + margin*(z+1)}
             size={size}
             zIndex={(10-y)*100 + (100-x) + z}
           />
@@ -41,8 +42,9 @@ const HyperCube = ({
 export default compose(
   defaultProps({
     depth: 5,
-    width: 5,
-    height: 5,
+    width: 4,
+    height: 3,
     size: 3,
+    margin: 0.25,
   }),
 )(HyperCube);
